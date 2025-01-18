062947a7be14a98437c5bae74265a1728a55f9a1
yixuan173 <testyixuan173@gmail.com>
2025-01-18 15:06:09 +0800

    feat: add RewardCardList

--- src/pages/RewardCardList/index.tsx
+++ src/pages/RewardCardList/index.tsx
@@ -1,5 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import { CardData } from '@/types/common';
+import { Tag, TagLabel } from '@chakra-ui/react';
+
 const RewardCardList = () => {
-  return <div>RewardCardList</div>;
+  const rewardCardList = localStorage.getItem('rewardCardList') || '[]';
+
+  return (
+    <div className="flex flex-col items-center h-screen overflow-auto">
+      <Link to="/">
+        <h1 className="text-5xl font-bold text-pink-400 mt-6">乖寶寶集點卡</h1>
+      </Link>
+      <section className="mt-12 w-screen grid grid-cols-2 gap-8 px-6">
+        {JSON.parse(rewardCardList).map((card: CardData) => (
+          <Link to={`/list/${card.id}`} key={card.id}>
+            <div className="w-full relative flex flex-col items-center">
+              <img src="/images/card.webp" alt={card.title} className="w-11/12" />
+              <h2 className="text-2xl font-bold text-pink-600">{card.title}</h2>
+              <Tag size="lg" colorScheme="red" borderRadius="full" className="absolute -top-3 -right-1">
+                <TagLabel>{card.currentPoints}</TagLabel>
+              </Tag>
+            </div>
+          </Link>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default RewardCardList;
