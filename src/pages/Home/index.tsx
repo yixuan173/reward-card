bf7164e2214efef7dafef0cd2f5ec6253d0909ec
yixuan173 <testyixuan173@gmail.com>
2025-01-18 12:49:22 +0800

    feat: add check card count function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,47 @@
-import { Button, Stack, useDisclosure } from '@chakra-ui/react';
+import { Button, Stack, useDisclosure, useToast } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 import CreateCardModal from './components/CreateCardModal';
+import { WarningTwoIcon } from '@chakra-ui/icons';
+
+const MAX_CARD_COUNT = 6;
 
 const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  const checkIsExceedCardMaxCount = () => {
+    const rewardCardList = localStorage.getItem('rewardCardList');
+    if (rewardCardList && JSON.parse(rewardCardList).length >= MAX_CARD_COUNT) return true;
+
+    return false;
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-14">
       <h1 className="text-5xl font-bold text-pink-400">乖寶寶集點卡</h1>
       <section>
         <Stack spacing="20px">
-          <Button colorScheme="pink" size="lg" onClick={onOpen}>
+          <Button
+            colorScheme="pink"
+            size="lg"
+            onClick={() => {
+              if (checkIsExceedCardMaxCount()) {
+                toast({
+                  title: '已達最大集點卡數量(6張)，無法繼續新增。',
+                  position: 'top',
+                  status: 'warning',
+                  duration: 2500,
+                  containerStyle: {
+                    marginTop: '2rem',
+                  },
+                  icon: <WarningTwoIcon mt="5px" />,
+                });
+                return;
+              }
+              onOpen();
+            }}
+          >
             建立集點卡
           </Button>
           <Link to="/list">
