ea31db7331ffbb521501aafd3db4bd094e9cb9c7
yixuan173 <testyixuan173@gmail.com>
2025-01-18 15:12:18 +0800

    feat: 調整 width css

--- src/pages/RewardCardList/index.tsx
+++ src/pages/RewardCardList/index.tsx
@@ -7,11 +7,11 @@ const RewardCardList = () => {
   const rewardCardList = localStorage.getItem('rewardCardList') || '[]';
 
   return (
-    <div className="flex flex-col items-center h-screen overflow-auto">
+    <div className="flex flex-col items-center h-screen">
       <Link to="/">
         <h1 className="text-5xl font-bold text-pink-400 mt-6">乖寶寶集點卡</h1>
       </Link>
-      <section className="mt-12 w-screen grid grid-cols-2 gap-8 px-6">
+      <section className="mt-12 w-full grid grid-cols-2 gap-8 px-6">
         {JSON.parse(rewardCardList).map((card: CardData) => (
           <Link to={`/list/${card.id}`} key={card.id}>
             <div className="w-full relative flex flex-col items-center">
