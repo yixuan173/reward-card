55954a455f614b9d8e003878e5366f8cad2517bd
yixuan173 <testyixuan173@gmail.com>
2025-02-05 15:08:45 +0800

    refactor: replace get card list function from localstorage to indexedDB

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,9 @@ import { Button, Stack, useDisclosure, useToast, Box, Link, Image } from '@chakr
 import { useNavigate } from 'react-router-dom';
 
 import CreateCardModal from './components/CreateCardModal';
-import { getItemFromLocalStorage } from '@util/localStorage';
-import type { CardData } from '@type/common';
-import { ALERT_STATUS, LOCAL_STORAGE_KEYS } from '@constants/index';
+import { ALERT_STATUS } from '@constants/index';
 import { showToast } from '@util/toast';
+import { getCardListFromIndexedDB } from '@/util/indexedDB';
 
 const MAX_CARD_COUNT = 6;
 const GITHUB_LINK = 'https://github.com/yixuan173/reward-card';
@@ -14,15 +13,24 @@ const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
   const navigate = useNavigate();
-  const rewardCardList = getItemFromLocalStorage(LOCAL_STORAGE_KEYS.REWARD_CARD_LIST) as CardData[];
 
-  const checkIsExceedCardMaxCount = () => {
+  const checkIsExceedCardMaxCount = async () => {
+    const rewardCardList = await getCardListFromIndexedDB();
     if (rewardCardList.length >= MAX_CARD_COUNT) return true;
-
     return false;
   };
 
-  const handleViewRewardCardList = () => {
+  const handleCreateCard = async () => {
+    const isExceedCardMaxCount = await checkIsExceedCardMaxCount();
+    if (isExceedCardMaxCount) {
+      showToast(toast, '已達最大集點卡數量(6張)，無法繼續新增。', ALERT_STATUS.WARNING);
+      return;
+    }
+    onOpen();
+  };
+
+  const handleViewRewardCardList = async () => {
+    const rewardCardList = await getCardListFromIndexedDB();
     if (rewardCardList.length > 0) {
       navigate('/list');
     } else {
@@ -36,17 +44,7 @@ const Home = () => {
         <h1 className="text-5xl font-bold text-pink-400">乖寶寶集點卡</h1>
         <section>
           <Stack spacing="20px">
-            <Button
-              colorScheme="pink"
-              size="lg"
-              onClick={() => {
-                if (checkIsExceedCardMaxCount()) {
-                  showToast(toast, '已達最大集點卡數量(6張)，無法繼續新增。', ALERT_STATUS.WARNING);
-                  return;
-                }
-                onOpen();
-              }}
-            >
+            <Button colorScheme="pink" size="lg" onClick={handleCreateCard}>
               新增集點卡
             </Button>
             <Button colorScheme="pink" size="lg" variant="outline" onClick={handleViewRewardCardList}>
