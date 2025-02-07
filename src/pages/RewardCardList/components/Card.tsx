210d5e4a05b344720799c69c7a9b251fc73da2c1
yixuan173 <testyixuan173@gmail.com>
2025-02-07 16:30:01 +0800

    feat: add card img skeleton

---
+++ src/pages/RewardCardList/components/Card.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Image, Tag, TagLabel } from '@chakra-ui/react';
+
+import getImageUrl from '@/util/getImageUrl';
+
+interface CardProps {
+  id: string;
+  title: string;
+  currentPoints: number;
+  cardImage: File | null;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  const { id, title, currentPoints, cardImage } = props;
+  const [loading, setLoading] = useState(true);
+
+  const onImgLoad = () => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  };
+
+  return (
+    <Link to={`/${id}`} key={id}>
+      <div className="w-full relative flex flex-col items-center">
+        <div className="relative">
+          <div
+            className={`absolute top-0 left-0 w-full h-64 bg-gray-200 animate-pulse ${loading ? 'block' : 'hidden'}`}
+          />
+          <Image
+            src={getImageUrl(cardImage) || './images/card.webp'}
+            alt={title}
+            objectFit="cover"
+            className={`w-full h-64 ${loading ? 'opacity-0' : 'opacity-100'}`}
+            onLoad={onImgLoad}
+          />
+        </div>
+        <h2 className="text-2xl font-bold text-pink-600">{title}</h2>
+        <Tag size="lg" colorScheme="red" borderRadius="full" className="absolute -top-3 -right-3">
+          <TagLabel>{currentPoints}</TagLabel>
+        </Tag>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
--- src/pages/RewardCardList/index.tsx
+++ src/pages/RewardCardList/index.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Image, Tag, TagLabel } from '@chakra-ui/react';
 
 import type { CardData } from '@type/common';
 import Header from '@components/Header';
 import { MAX_CARD_COUNT } from '@constants/index';
 import { getCardListFromIndexedDB } from '@util/indexedDB';
-import getImageUrl from '@/util/getImageUrl';
+import Card from './components/Card';
 
 const RewardCardList = () => {
   const [cardList, setCardList] = useState<CardData[]>([]);
@@ -29,20 +27,7 @@ const RewardCardList = () => {
         </span>
         <div className="grid grid-cols-2 gap-8 mt-2">
           {cardList.map(({ id, title, currentPoints, cardImage }: CardData) => (
-            <Link to={`/${id}`} key={id}>
-              <div className="w-full relative flex flex-col items-center">
-                <Image
-                  src={getImageUrl(cardImage) || './images/card.webp'}
-                  alt={title}
-                  objectFit="cover"
-                  className="w-full h-64"
-                />
-                <h2 className="text-2xl font-bold text-pink-600">{title}</h2>
-                <Tag size="lg" colorScheme="red" borderRadius="full" className="absolute -top-3 -right-3">
-                  <TagLabel>{currentPoints}</TagLabel>
-                </Tag>
-              </div>
-            </Link>
+            <Card key={id} id={id} title={title} currentPoints={currentPoints} cardImage={cardImage} />
           ))}
         </div>
       </section>
